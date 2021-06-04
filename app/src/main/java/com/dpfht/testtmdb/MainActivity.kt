2bf47a361f2bd64c122f4ff0b71856acc299c4fb
Dhipa Poltak <dhipa.poltak@alfacart.com>
2021-06-04 20:12:15 +0700

    removed old API AsyncTask

--- app/src/main/java/com/dpfht/testtmdb/MainActivity.kt
+++ app/src/main/java/com/dpfht/testtmdb/MainActivity.kt
@@ -1,40 +1,45 @@
 package com.dpfht.testtmdb
 
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dpfht.testtmdb.activity.GenreActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.Request
-
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    /*
-    AsyncTask.execute {
-      val hostname = "api.themoviedb.org"
-      val certificatePinner = CertificatePinner.Builder()
-              .add(hostname, "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
-              .build()
-      val client = OkHttpClient.Builder()
-              .certificatePinner(certificatePinner)
-              .build()
+    val isTest = false
+    if (isTest) {
+      GlobalScope.launch {
+        testHit()
+      }
+    } else {
+      val itn = Intent(this, GenreActivity::class.java)
+      startActivity(itn)
 
-      val request: Request = Request.Builder()
-              .url("https://$hostname")
-              .build()
-      client.newCall(request).execute()
+      finish()
     }
-    */
+  }
 
-    val itn = Intent(this, GenreActivity::class.java)
-    startActivity(itn)
+  private fun testHit() {
+    val hostname = "api.themoviedb.org"
+    val certificatePinner = CertificatePinner.Builder()
+      .add(hostname, "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
+      .build()
+    val client = OkHttpClient.Builder()
+      .certificatePinner(certificatePinner)
+      .build()
 
-    finish()
+    val request: Request = Request.Builder()
+      .url("https://$hostname")
+      .build()
+    client.newCall(request).execute()
   }
 }

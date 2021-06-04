c542edf31575ef8a40fe1652383f84a69ef6fee4
Dhipa Poltak <dhipa.poltak@alfacart.com>
2021-06-04 19:49:25 +0700

    removed old API AsyncTask

--- app/src/main/java/com/dpfht/testtmdb/MainActivity.kt
+++ app/src/main/java/com/dpfht/testtmdb/MainActivity.kt
@@ -1,40 +1,59 @@
 package com.dpfht.testtmdb
 
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dpfht.testtmdb.activity.GenreActivity
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.Request
-
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    /*
-    AsyncTask.execute {
-      val hostname = "api.themoviedb.org"
-      val certificatePinner = CertificatePinner.Builder()
-              .add(hostname, "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
-              .build()
-      val client = OkHttpClient.Builder()
-              .certificatePinner(certificatePinner)
-              .build()
+    val isTest = false
+    if (isTest) {
+      Observable.empty<String>()
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
+        .subscribe(object : Observer<Any> {
+          override fun onSubscribe(d: Disposable) {}
+          override fun onNext(value: Any) {}
 
-      val request: Request = Request.Builder()
-              .url("https://$hostname")
-              .build()
-      client.newCall(request).execute()
+          override fun onError(e: Throwable) {
+            e.printStackTrace()
+          }
+
+          override fun onComplete() {
+            testHit()
+          }
+        })
+    } else {
+      val itn = Intent(this, GenreActivity::class.java)
+      startActivity(itn)
+
+      finish()
     }
-    */
+  }
 
-    val itn = Intent(this, GenreActivity::class.java)
-    startActivity(itn)
+  private fun testHit() {
+    val hostname = "api.themoviedb.org"
+    val certificatePinner = CertificatePinner.Builder()
+      .add(hostname, "sha256/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=")
+      .build()
+    val client = OkHttpClient.Builder()
+      .certificatePinner(certificatePinner)
+      .build()
 
-    finish()
+    val request: Request = Request.Builder()
+      .url("https://$hostname")
+      .build()
+    client.newCall(request).execute()
   }
 }
